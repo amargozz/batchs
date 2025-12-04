91e393328d41a7c4d61c25b646d0edc3fe4d9801
amargozz <malexgope@gmail.com>
2025-12-04 11:33:23 -0600

    Update automatic_copy_public_folder.bat

--- automatic_copy_public_folder.bat
+++ automatic_copy_public_folder.bat
@@ -14,10 +14,11 @@ pause
 
 for /f "tokens=*" %%i in ('dsquery computer -name %prefix%*') do (
     set "dn=%%i"
-    rem Extraer el nombre del equipo desde el DN
+    ::EXTRACT COMPUTER NAME FROM CN
     for /f "tokens=2 delims==," %%a in ("!dn!") do (
         set "computer=%%a"
-        echo ping !computer!...
+
+        ::PING COMPUTER
         ping -n 1 !computer! | find "TTL=" >nul
         if !errorlevel! == 0 (
             set /a activeCount+=1
@@ -25,23 +26,24 @@ for /f "tokens=*" %%i in ('dsquery computer -name %prefix%*') do (
             ::TARGET FULL PATH
             set "target=\\!computer!\C$\Users\Public\Desktop\PUBLIC_FOLDER_NAME"
 
-            rem Crear carpeta destino si no existe
+           ::CREATING FOLDER IF NO EXIST
             if not exist "!destino!" (
                 echo !computer! - folder does not exist, creating...
                 mkdir "!target!"
             )
 
-            rem Copiar archivos
-            echo Copiando accesos a !computer!...
+            ::COPY FILES FROM SRC TO DST
+            echo [OK ] !computer! - Copying files 
             xcopy "%source%\*" "!target!\" /E /Y /I
         )
     )
 )
 
 echo.
-echo Total de equipos activos procesados: %activeCount%
+echo Total completed: %activeCount%
 
 endlocal
 
 pause
+
 
