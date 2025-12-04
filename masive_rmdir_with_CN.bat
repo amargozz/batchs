34bc9b0c701fda015e75fc95082dcddc92dba5ca
amargozz <malexgope@gmail.com>
2025-12-04 10:34:57 -0600

    Update masive_rmdir_with_CN.bat
    more clear

--- masive_rmdir_with_CN.bat
+++ masive_rmdir_with_CN.bat
@@ -2,7 +2,7 @@
 setlocal enabledelayedexpansion
 
 set "prefix=PREFIX"
-set "publicFolder=DIR_NAME"
+set "publicFolder=FOLDER_NAME"
 set "dryRun=1"
 
 set "activeCount=0"
@@ -19,23 +19,18 @@ for /f "usebackq delims=" %%i in (`dsquery computer -name %prefix%*`) do (
 
     set /a totalCount+=1
 
-    :: Quitar comillas
+    :: EXTRACT CN
     set "dn=%%i"
     set "dn=!dn:"=!"
-
-    :: Extraer CN antes de la primera coma
     for /f "tokens=1 delims=," %%a in ("!dn!") do (
-        set "cn=%%a"
-    )
-
-    :: QUITAR CN= DE FORMA SEGURA
+        set "cn=%%a")
     set "computer=!cn!"
     if /i "!computer:~0,3!"=="CN=" set "computer=!computer:~3!"
 
     :: PING
     ping -n 1 !computer! | find "TTL=" >nul
     if errorlevel 1 (
-        echo [UNREACHABLE] !computer! - skkiped.
+        echo [UNREACHABLE] !computer! - skipped.
     ) else (
 
         set /a activeCount+=1
@@ -44,19 +39,18 @@ for /f "usebackq delims=" %%i in (`dsquery computer -name %prefix%*`) do (
 
         if exist "!target!" (
             if "%dryRun%"=="1" (
-                echo [DRY RUN] target found: "!target!"
+                echo [DRY RUN] !computer! - target found: "!target!"
             ) else (
-                echo removing...
                 rmdir /S /Q "!target!"
                 if errorlevel 0 (
-                    echo [OK] target removed
+                    echo [OK] !computer! - target removed
                     set /a deletedCount+=1
                 ) else (
-                    echo [ERROR] can't remove target
+                    echo [ERROR] !computer! - can't remove target
                 )
             )
         ) else (
-            echo Target does not exist.
+            echo !computer! Target does not exist.
         )
 
     )
@@ -67,7 +61,7 @@ echo ==================================================
 echo Total nodes: %totalCount%
 echo Online: %activeCount%
 if "%dryRun%"=="1" (
-    echo DRY RUN: Nothing has been removed :)
+    echo DRY RUN: Nothing has been removed
 ) else (
     echo Targets removed: %deletedCount%
 )
